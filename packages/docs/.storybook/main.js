bcc4dce8d74a6fda3b458c5a2d71fe2b1cbab79b
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-28 17:14:43 -0300

    feat: create page for home

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-  stories: [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx",
-  ],
+  stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
