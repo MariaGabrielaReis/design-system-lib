db36f559cfd3cda945083301eaa8c6959b9a1fd2
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-28 09:32:54 -0300

    feat: install react

--- packages/ui/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from "@maby-ui/tokens";
-
-console.log(colors);
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from "@maby-ui/tokens";
+
+export function App() {
+  return <h1 style={{ color: colors.teal600 }}>Hello world!</h1>;
+}
