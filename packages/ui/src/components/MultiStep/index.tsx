42ed0b4d7b11a75d29e0186a2d310ab5aee44cbb
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-30 11:07:43 -0300

    feat: create multi step component

---
+++ packages/ui/src/components/MultiStep/index.tsx
@@ -0,0 +1,21 @@
+import { MultiStepContainer, Label, Steps, Step } from "./styles";
+
+export interface MultiStepProps {
+  steps: number;
+  currentStep?: number;
+}
+
+export function MultiStep({ steps, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {steps}
+      </Label>
+      <Steps css={{ "--steps-number": steps }}>
+        {Array.from({ length: steps }, (_, index) => index + 1).map(step => {
+          return <Step key={step} active={currentStep >= step} />;
+        })}
+      </Steps>
+    </MultiStepContainer>
+  );
+}
