4ba8695bc088eac36db5509766c87d24d918df33
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-30 13:23:31 -0300

    ci: add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -13,4 +13,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/design-system-lib/";
+    }
+
+    return config;
+  },
 };
